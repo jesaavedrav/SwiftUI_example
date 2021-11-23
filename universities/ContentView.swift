2e80e610e1d12d0c2f89fdadcb228e013da586d1
Jorge Saavedra <jsaavedra@montran.com>
2021-11-23 12:11:27 -0500

    Initial Commit

---
+++ universities/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  universities
+//
+//  Created by Jorge Saavedra on 23/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ universities/universitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  universitiesApp.swift
+//  universities
+//
+//  Created by Jorge Saavedra on 23/11/21.
+//
+
+import SwiftUI
+
+@main
+struct universitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
